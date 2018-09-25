228b443dc9b842361eb416885d23e0b043cfc68c
dariogarciag <dario.garcia@yogome.com>
2018-09-25 12:49:46 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DetailsComponent } from './components/details/details.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UsersComponent } from './components/users/users.component';
+
+const routes: Routes = [
+  {path: '', component: UsersComponent},
+  {path: 'details/:id', component: DetailsComponent},
+  {path: 'posts', component: PostsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
